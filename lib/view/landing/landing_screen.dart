eef014e7c03180444c0b504c065de5acc36de7a0
Ajeet Kumar Sah <aminemshaw@gmail.com>
2024-07-06 14:39:12 +0530

    landing page chages

--- lib/view/landing/landing_screen.dart
+++ lib/view/landing/landing_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:new_app/model/post_model.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:new_app/controller/landing_controller.dart';
 
@@ -27,36 +28,35 @@ class LandingScreen extends StatelessWidget {
                     strokeWidth: 1,
                   ),
                 )
-              : SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      ...ctlr.posts.map(
-                        (item) => Container(
-                          padding: const EdgeInsets.all(12),
-                          color: Colors.teal[900],
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 12, vertical: 8),
-                          child: ListTile(
-                            title: Text(
-                              item.title ?? '',
-                              style: GoogleFonts.ptSansCaption(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            subtitle: Text(
-                              item.body ?? '',
-                              style: GoogleFonts.ptSansCaption(
-                                fontSize: 12,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300,
-                              ),
-                            ),
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: ctlr.posts.length,
+                  itemBuilder: (_, i) {
+                    PostModel item = ctlr.posts[i];
+                    return Container(
+                      padding: const EdgeInsets.all(12),
+                      color: Colors.teal[900],
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 8),
+                      child: ListTile(
+                        title: Text(
+                          item.title ?? '',
+                          style: GoogleFonts.ptSansCaption(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        subtitle: Text(
+                          item.body ?? '',
+                          style: GoogleFonts.ptSansCaption(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
         );
       },

571e31e634aaca05530d98fb0f1b2c641844e047
Ajeet Kumar Sah <aminemshaw@gmail.com>
2024-07-10 14:49:30 +0530

    default firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:new_app/utils/routes/app_pages.dart';
-
 import 'bindings/bindings.dart';
+import 'package:flutter/material.dart';
+import 'package:new_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:new_app/utils/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize Firebase SDK
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // await Firebase.initializeApp();
   HomeBindings().dependencies();
   runApp(const MyApp());
 }
